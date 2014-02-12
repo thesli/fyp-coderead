47de8034f7f0a0874bf2b521d4292aea03e22217
sliversniper <sliv.phone@gmail.com>
2014-02-12 10:59:46 +0000

    added some comment on database.sql

--- database.sql
+++ database.sql
@@ -12,3 +12,8 @@ CREATE TABLE users (
   activated enum('0','1') NOT NULL default '0',
   PRIMARY KEY  (userid)
 ) TYPE=MyISAM COMMENT='Membership Information';
+-- email_address 25 char? you serious bro?
+-- a lot of time you want unique instead of not null.
+-- activated should be a boolean value instead of enum 1,0
+-- user_level should not enum 1,2,3 make it meaningful
+-- why info should be not NULL
